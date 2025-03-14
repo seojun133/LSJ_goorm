3844bb3c163ce44cb3b3ad6636aff1bfbc221679
Seojun Lee <epqlf9999@gmail.com>
2025-03-14 14:20:51 +0900

    판다스 실습을 위함

---
+++ pandas/pandas_silseup2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+np_arr = np.array([1, 2, 3, 4])
+pd_series = pd.Series([1, 2, 3, 4])
+
+print("np_arr: ", np_arr)
+print("np_arr shape: ", np_arr.shape)
+print("********")
+print("pd_series:\n", pd_series)
+print("pd_series shape: ", pd_series.shape)
