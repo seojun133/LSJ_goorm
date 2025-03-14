c0cfccf4723ef01884ba5ac7561ec4bc3bf24269
Seojun Lee <epqlf9999@gmail.com>
2025-03-14 09:36:53 +0900

    넘파이 실습을 위함

---
+++ numpy/numpy_silesup.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+# data1 = [ 0, 1, 2, 3, 4, 5 ]
+# a1 = np.array(data1)
+# print(a1)
+
+# data2 = [0.1, 5, 4, 12, 0.5]
+# a2 = np.array(data2)
+# print(a2)
+
+# a3 = np.arange(12).reshape(4,3)
+# print(a3)
+
+# data2 = np.array([1,2,3])
+# print(data2.shape)
+# print(data2.dtype)
+
+# data3 = np.array([[1,2,3], [4,5,6]])
+# print(data3.shape)
+# print(data3.dtype)
+
+# x1 = [1, 2, 3, 4, 5]
+# x2 = [6, 7, 8, 9, 10]
+# data4 = np.array(x1)
+# data5 = np.array([x1, x2])
+# print(data4, data4.shape, data4.ndim, data4.dtype)
+# print(data5, data5.shape, data5.ndim, data5.dtype)
+
+# print(np.arange(2, 10, 2))
+# print(np.zeros(4))
+# print(np.zeros((3, 2)))
+# print(np.eye(3))
+
+# x3 = np.array([1,2,3,4,5])
+# x4 = np.array([6,7,8,9,10])
+# print(x3 * 2)
+# print(x3 + x4)
+
+x5 = np.array([1,2,3,4,5])
+print(x5)
