2c4439ef34328cd2b5ece75a90ea7afd95dc3e92
Seojun Lee <epqlf9999@gmail.com>
2025-03-14 15:37:31 +0900

    연습장

--- goorm_coding/hi.py
+++ goorm_coding/hi.py
@@ -7,5 +7,14 @@
 # b = float(a)
 # print(type(b))
 
-if y == 3:
-    print(not y)
+# if y == 3:
+#     print(not y)
+
+N = int(input())
+i=1
+for i in range(N):
+	i += 1
+	sum = 1
+	sum += sum * i 
+	
+print(sum)
