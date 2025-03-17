8fbec90bcf544498814c296df834659ea999646e
Seojun Lee <epqlf9999@gmail.com>
2025-03-17 17:48:38 +0900

    .

--- goorm_coding/hi.py
+++ goorm_coding/hi.py
@@ -10,11 +10,19 @@
 # if y == 3:
 #     print(not y)
 
-N = int(input())
-i=1
-for i in range(N):
-	i += 1
-	sum = 1
-	sum = sum * i 
+# N = int(input())
+# i=1
+# for i in range(N):
+# 	i += 1
+# 	sum = 1
+# 	sum = sum * i 
 	
-print(sum)
+# print(sum)
+
+N = int(input())
+S = list(map(int, input().split()))
+sorted_score = sorted(S)[-1:-4:-1]
+# for i in range(N):
+#     if S[i] in sorted_score:
+#         print(i)
+print(sorted_score)
