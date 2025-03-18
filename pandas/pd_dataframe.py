7f2cee5ea43a755b5dc942aeec5bc2be14ec7b96
Seojun Lee <epqlf9999@gmail.com>
2025-03-18 17:38:35 +0900

    데이터프레임 실습

--- pandas/pd_dataframe.py
+++ pandas/pd_dataframe.py
@@ -19,7 +19,8 @@ df = DataFrame(data, index = ['mid', 'report', 'final'], columns=['kim', 'lee', 
 # print('행 색인:', df.index)
 # print('열 색인:', df.columns)
 
-print(df)
+# print(df)
+
 # print('*********')
 # print('행의 이름으로 검색: ',df.loc[['mid', 'final']])
 # print('*********')
@@ -28,12 +29,41 @@ print(df)
 # print('열 인덱싱:\n', df['kim'])
 # print('복수 열 인덱싱:\n', df[['kim', 'lee']])
 
-print(df.loc[:, 'lee':'park'])
-print(df.iloc[:,:2])
+# print(df.loc[:, 'lee':'park'])
+# print(df.iloc[:,:2])
 
-bonus = Series([3, 5, 2], index=['mid', 'report', 'final'])
-df['bon'] = bonus
-print(df)
+# bonus = Series([3, 5, 2], index=['mid', 'report', 'final'])
+# df['bon'] = bonus
+# print(df)
 
-print(df.iloc[1]['lee']) # Series
-print(df.iloc[[1]][['lee']]) # DataFrame
+# print(df.iloc[1]['lee']) # Series
+# print(df.iloc[[1]][['lee']]) # DataFrame
+
+df2 = df.T
+# print(df2)
+# print(df2[df2['mid'] > 80])
+# print (df2[(df2['mid'] > 80) & (df2['final'] > 80)])
+
+
+s1 = Series([1, 2, 3, 4], index=['case1', 'case2', 'case3', 'case4'])
+s2 = Series([9, 8, 7, 6, 5], index= ['case5', 'case4', 'case3', 'case2', 'case1'])
+# print(s1)
+# print('*********')
+# print(s2)
+
+# print(s1+s2) # 한 쪽이 결측값이면 전체가 결측값이 발생 fill_value 사용 but
+# print(s1.add(s2, fill_value=0)) # 서로 더하려면 add 함수를 사용
+
+data3 = {'mid':[80, 90, 85], 'report':[10, 8,9], 'final':[85, 70, 90]}
+data4 = {'mid':[70, 80, 95, 90], 'report':[9, 7, 8, 9], 'final':[95, 80, 70, 80]}
+
+df3 = DataFrame(data3, index=['Kim', 'Lee', 'Park'])
+df4 = DataFrame(data4, index=['Kim', 'Lee', 'Park', 'Choi'])
+
+# print(df3)
+# print('**********')
+# print(df4)
+
+# print(df3 + df4)
+# print(df3.add(df4, fill_value=0))
+
