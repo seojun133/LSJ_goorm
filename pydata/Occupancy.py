b06d60743fb092c831a9d36b60c02ec5b61ba5af
Seojun Lee <epqlf9999@gmail.com>
2025-03-18 17:39:03 +0900

    .

---
+++ pydata/Occupancy.py
@@ -0,0 +1,7 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("/Users/goorm/Documents/GitHub/LSJ_goorm/pydata/occupancy.csv")
+df.head()
+print(df)
